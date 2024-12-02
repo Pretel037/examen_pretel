3dec782b6d1103ac757c62b47bb7eb10cd1a87ba
Pretel037 <113481698+Pretel037@users.noreply.github.com>
2024-12-02 09:22:49 -0500

    Update examen_pretel1.sql

--- examen_pretel1.sql
+++ examen_pretel1.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS Examen_PRETEL1;
-USE Examen_PRETEL1
-CREATE TABLE autor (
+CREATE SCHEMA IF NOT EXISTS EXAMEN;
+USE EXAMEN;
+CREATE TABLE Autor (
   idautor INT  NOT NULL PRIMARY KEY
   pais_origen VARCHAR(50),
   ano_nacimiento VARCHAR(50),
@@ -8,9 +8,7 @@ CREATE TABLE autor (
   nombre VARCHAR(50),
   año_defuncion VARCHAR(50)
 );
-
-
-INSERT INTO autor (id_autor, pais_origen, año_nacimiento, sexo, nombre, año_defuncion)
+INSERT INTO Autor (id_autor, pais_origen, año_nacimiento, sexo, nombre, año_defuncion)
 VALUES
 (1,'Peru','1966','hombre','Juan','2020'),
 (2,'Peru','1976','hombre','Felipe','2024'),
@@ -18,13 +16,13 @@ VALUES
 (4,'Peru','1969','hombre','Pedro','2024'),
 (5,'Peru','1986','mujer','Jenny','2012')
 
-CREATE TABLE libro (
+CREATE TABLE Libro (
   idlibro INT  NOT NULL PRIMARY KEY
   titulo VARCHAR(255),
   año_publicacion VARCHAR(255)
 );
 
-INSERT INTO libro (id_libro, titulo, año_publicacion)
+INSERT INTO Libro (id_libro, titulo, año_publicacion)
 VALUES
 (1,'Machine learning 1','1966'),
 (2,'Deep learning','2000'),
@@ -32,14 +30,6 @@ VALUES
 (4,'Apache 4','2002'),
 (5,'Docker','2003')
 
-
-CREATE TABLE escribir(
-  id_escribir INT PRIMARY KEY,
-  idautor INT,
-  idlibro INT,
-  FOREIGN KEY (id_autor) REFERENCES autor(Nombre_departamento),
-  FOREIGN KEY (id_libro) REFERENCES libro(titulo)
-);
 
 
 
