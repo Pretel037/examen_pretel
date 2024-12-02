cf581de615a48635108a19e007b67c92201b065d
Pretel037 <113481698+Pretel037@users.noreply.github.com>
2024-12-02 09:59:24 -0500

    Update examen_pretel6.sql

--- examen_pretel6.sql
+++ examen_pretel6.sql
@@ -9,11 +9,11 @@ CREATE TABLE Autor (
   ano_defuncion VARCHAR(50)
 );
 
-INSERT INTO Autor (1,'Peru','1966','hombre','Juan','2020'),
-INSERT INTO Autor (2,'Peru','1976','hombre','Felipe','2024'),
-INSERT INTO Autor (3,'Peru','1988','mujer','Laura','2021'),
-INSERT INTO Autor (4,'Peru','1969','hombre','Pedro','2024'),
-INSERT INTO Autor (5,'Peru','1986','mujer','Jenny','2012'),
+INSERT INTO Autor VALUES (1,'Peru','1966','hombre','Juan','2020'),
+INSERT INTO Autor VALUES (2,'Peru','1976','hombre','Felipe','2024'),
+INSERT INTO Autor VALUES (3,'Peru','1988','mujer','Laura','2021'),
+INSERT INTO Autor VALUES (4,'Peru','1969','hombre','Pedro','2024'),
+INSERT INTO Autor VALUES (5,'Peru','1986','mujer','Jenny','2012'),
 
 CREATE TABLE Libro (
   idlibro INT  NOT NULL PRIMARY KEY,
@@ -21,8 +21,8 @@ CREATE TABLE Libro (
   publica VARCHAR(255)
 );
 
-INSERT INTO Libro (1,'Machine learning 1','1966'),
-INSERT INTO Libro (2,'Deep learning','2000'),
-INSERT INTO Libro (3,'Cubit 3','2001'),
-INSERT INTO Libro (4,'Apache 4','2002'),
-INSERT INTO Libro (5,'Docker','2003'),
+INSERT INTO Libro VALUES (1,'Machine learning 1','1966'),
+INSERT INTO Libro VALUES (2,'Deep learning','2000'),
+INSERT INTO Libro VALUES (3,'Cubit 3','2001'),
+INSERT INTO Libro VALUES (4,'Apache 4','2002'),
+INSERT INTO Libro VALUES (5,'Docker','2003'),
